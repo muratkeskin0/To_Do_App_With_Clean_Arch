2f8288992eaa90f1950a49b324feebf756a489f0
Murat Keskin <eng.murat.keskin@gmail.com>
2024-08-29 20:41:39 +0300

    .

--- LastToDoApp/ToDoApp.Contracts/obj/Debug/net8.0/ToDoApp.Contracts.AssemblyInfo.cs
+++ LastToDoApp/ToDoApp.Contracts/obj/Debug/net8.0/ToDoApp.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f580267d2148e9b8b5668e479f46fbe51b50a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
